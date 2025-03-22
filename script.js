39f5391958be3f45caaba8add4dfee4fb2237a78
technusEnzo <enzojonty@gmail.com>
2025-03-22 21:48:04 +0200

    Added a clear button + multiply by 2 button.
    The clear button clears the count tally.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let count = 0;
 const counterDisplay = document.getElementById('counter');
 const incrementButton = document.getElementById('incrementButton');
 const decrementButton = document.getElementById('decrementButton');
+const multiplyCounterButton = document.getElementById('multiplyByTwo')
+const clearButton = document.getElementById('clearButton');
 
 incrementButton.addEventListener('click', () => {
     count++;
@@ -10,5 +12,15 @@ incrementButton.addEventListener('click', () => {
 
 decrementButton.addEventListener('click', () => {
     count--;
+    counterDisplay.textContent = count;
+});
+
+multiplyCounterButton.addEventListener('click', () => {
+    count = count * 2;
+    counterDisplay.textContent = count;
+});
+
+clearButton.addEventListener('click', () => {
+    count = 0;
     counterDisplay.textContent = count;
 });
